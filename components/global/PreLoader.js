12e4e5aba44bc1eeeacfdaee8ac67e824a8decb9
Mohamed Nidal <touhami9@gmail.com>
2021-07-28 12:33:32 +0100

    Add Base Components

---
+++ components/global/PreLoader.js
@@ -0,0 +1,11 @@
+const PreLoader = (params) => {
+    return (
+        <>
+            {/* Page Preloder */}
+            <div id="preloder">
+                <div className="loader" />
+            </div>
+        </>
+    );
+};
+export default PreLoader;
---
+++ pages/_document.js
@@ -0,0 +1,35 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Footer from '../components/global/Footer'
+import Header from '../components/global/Header'
+import MainLinks from '../components/global/MainLinks'
+import MainScripts from '../components/global/MainScripts'
+import PreLoader from '../components/global/PreLoader'
+import Sidebar from '../components/global/Sidebar'
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx)
+        return { ...initialProps }
+}
+
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <MainLinks />
+                </Head>
+                <body>
+                    <PreLoader />
+                    <Sidebar />
+                    <Header />
+                    <Main />
+                    <Footer />
+                    <MainScripts />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument;
