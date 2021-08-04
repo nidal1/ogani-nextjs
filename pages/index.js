441af46a28dab26d06204f11965f0c5f6f2496fe
Mohamed Nidal <touhami9@gmail.com>
2021-08-04 17:04:51 +0100

    Add Base TO Home Page

--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,18 @@ import Categories from "../components/home/sections/Categories";
 import Featured from "../components/home/sections/Featured";
 import Hero from "../components/home/sections/Hero";
 import LatestProduct from "../components/home/sections/LatestProduct";
+import Base from '../components/global/Base'
 
-export default function Home({value="customAlt"}) {
+
+export default function Home({ value = "customAlt" }) {
   return (
-    <>
+    <Base>
       <Hero />
       <Categories />
       <Featured />
       <Banner />
       <LatestProduct />
       <Blog />
-    </>
+    </Base>
   )
 }
