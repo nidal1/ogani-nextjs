ce94b3254d042462f8f64a707b3ba497c0cf1329
Mohamed Nidal <touhami9@gmail.com>
2021-08-04 16:24:12 +0100

    Add Shopping Cart

---
+++ pages/shoping-cart/index.js
@@ -0,0 +1,9 @@
+import Base from '../../components/global/Base';
+
+export default function ShopingCart(params) {
+    return (
+        <Base>
+            <h1>Shoping cart</h1>
+        </Base>
+    )
+}
