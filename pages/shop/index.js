bf500a14cce8d2154e23431831f9ec7cf330dcbf
Mohamed Nidal <touhami9@gmail.com>
2021-08-04 16:22:59 +0100

    Add Shop Page

---
+++ pages/shop/index.js
@@ -0,0 +1,9 @@
+import Base from '../components/global/Base';
+
+export default function Shop({ value = "customAlt" }) {
+    return (
+        <Base>
+            <h1>Shop</h1>
+        </Base>
+    );
+}
