505b3c9fa992aea96064df5bfb761012fe042dc6
Mohamed Nidal <touhami9@gmail.com>
2021-08-30 21:27:09 +0100

    Fetching Data From Server

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/global.css'
+import { Provider } from 'react-redux';
+import store from '../store'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { fetchData } from '../util';
+import '../styles/global.css';
+
+
+function MyApp({ Component, pageProps, data }) {
+  
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} data = { data }/>
+    </Provider>
+  )
+}
+
+MyApp.getInitialProps = async (ctx) => {
+  const data = await fetchData('http://localhost:3000/api/getAllProducts');
+  return { data : [...data] }
 }
 
 export default MyApp
