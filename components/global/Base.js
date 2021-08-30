52dabf98f94581548c04cee5c034f0a81fde75e8
Mohamed Nidal <touhami9@gmail.com>
2021-08-30 21:25:55 +0100

    Modifing Base From Global Components To HOC

--- components/global/Base.js
+++ components/global/Base.js
@@ -1,23 +1,35 @@
-
 import Sidebar from './Sidebar';
 import Header from './Header'
 import Hero from './Hero';
 import Breadcrumb from './Hero/Breadcrumb';
 
 import { useRouter } from 'next/dist/client/router';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import { addProducts } from '../../features/products/productsSlice';
 
 
-const Base = ({ children }) => {
-    const pathname = useRouter().pathname;
-    
-    return (
-        <>
-            <Sidebar />
-            <Header />
-            <Hero />
-            { pathname !== '/' && <Breadcrumb />}
-            {children}
-        </>
-    );
-};
+
+const Base = (Component) => {
+    return ({ data }) => {
+        const pathname = useRouter().pathname;
+        const products = useSelector(state => state);
+        const dispatch = useDispatch();
+        
+        useEffect(() => {
+            dispatch(addProducts(data));
+        })
+        return (
+            <>
+                <Sidebar />
+                <Header />
+                <Hero />
+                {pathname !== '/' && <Breadcrumb />}
+                <Component />
+            </>
+        );
+    }
+}
+
+
 export default Base;
